bd34caa81fc357d7806b8deb480a7a38b356f1e9
Smruti Sagar Pattanaik <sspwebdev71@gmail.com>
2025-07-20 11:34:00 +0530

    restructure folders

--- CPURaytracer/index.js
+++
@@ -1,4 +0,0 @@
-import { RaytracingManager } from "./src/RaytracingManager.js";
-import { CanvasManager } from "./src/CanvasManager.js";
-
-export { RaytracingManager, CanvasManager };
---
+++ types/index.d.ts
@@ -0,0 +1,19 @@
+import {
+  Vector3,
+  Color,
+  Pixel,
+  EnableMouseMovementsArgs,
+} from "./TypesAndInterfaces.ts";
+import { RaytracingManagerProps } from "./TypesAndInterfaces.ts";
+import { RaytracingManager } from "../src/RaytracingManager.js";
+import { CanvasManager } from "../src/CanvasManager.js";
+
+export {
+  Vector3,
+  Color,
+  Pixel,
+  EnableMouseMovementsArgs,
+  RaytracingManagerProps,
+  RaytracingManager,
+  CanvasManager,
+};
